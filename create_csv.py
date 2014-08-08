dfa62d1048ef9b696836c8d7123c66966b13b173
cardwizard <aadeshbagmar@gmail.com>
2014-08-08 22:58:25 +0530

    Create create_csv.py

---
+++ create_csv.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+import os.path
+
+# This is a tiny script to help you creating a CSV file from a face
+# database with a similar hierarchie:
+#
+#  philipp@mango:~/facerec/data/at$ tree
+#  .
+#  |-- README
+#  |-- s1
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#  |-- s2
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#  ...
+#  |-- s40
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#
+
+if __name__ == "__main__":
+
+    if len(sys.argv) != 3:
+        print "usage: create_csv <base_path> name"
+        sys.exit(1)
+
+    BASE_PATH=sys.argv[1]
+    SEPARATOR=";"
+    FILE_NAME= sys.argv[2]
+    file=open(FILE_NAME,"w+");
+    label = 0
+    for dirname, dirnames, filenames in os.walk(BASE_PATH):
+        for subdirname in dirnames:
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                abs_path = "%s/%s" % (subject_path, filename)
+                file.write("%s%s%s\n" % (abs_path, SEPARATOR, label))
+                #print "%s%s%d" % (abs_path, SEPARATOR, label)
+            label = label + 1
+file.close()
